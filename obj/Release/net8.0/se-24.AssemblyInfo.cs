bfb1bda2a7458b21de260d5983078a68c33e1677
MrNugs <lukasmacevicius03041@gmail.com>
2024-09-25 15:30:56 +0300

    Added Reading game functionality

--- obj/Release/net8.0/se-24.AssemblyInfo.cs
+++ obj/Release/net8.0/se-24.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("se-24")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f5a238d00b210539168ccf7d780faedf2ac5321")]
 [assembly: System.Reflection.AssemblyProductAttribute("se-24")]
