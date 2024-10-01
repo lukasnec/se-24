b2a2dff66c0f991d86a551a933d8a414b6080d89
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-10-01 23:47:05 +0300

    Created backend for object finder game

---
+++ src/Games/FinderGame/Level.cs
@@ -0,0 +1,12 @@
+﻿namespace src.Games.FinderGame
+{
+    public class Level
+    {
+        public int id { get; set; }
+        public string image { get; set; }
+
+        public int givenTime { get; set; }
+
+        public List<GameObject> gameObjects { get; set; } = [];
+    }
+}
