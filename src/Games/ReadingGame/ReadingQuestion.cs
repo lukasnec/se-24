e3ff1307edb691ecf8f99741cfc0bbd5b8e8a93d
MrNugs <lukasmacevicius03041@gmail.com>
2024-10-16 17:59:48 +0300

    Changed class names

--- src/Games/ReadingGame/ReadingQuestion.cs
+++ src/Games/ReadingGame/ReadingQuestion.cs
@@ -1,6 +1,6 @@
 ﻿namespace src.Games.ReadingGame
 {
-    public class QuestionClass
+    public class ReadingQuestion
     {
         public string Question { get; set; } = "";
         public string[] Answers { get; set; } = new string[4];
