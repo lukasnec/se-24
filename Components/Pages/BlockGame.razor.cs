68280c20301bbbfa7c13307ee75c5fe59edc7fcf
lukasnec <lneciunskas@gmail.com>
2024-10-23 17:35:22 +0300

    Added Record functionality

--- Components/Pages/BlockGame.razor.cs
+++ Components/Pages/BlockGame.razor.cs
@@ -7,32 +7,15 @@ using System.Threading.Tasks;
 
 namespace se_24.Components.Pages
 {
-    public class GameMove
-    {
-        public int SquareId { get; set; }
+    public record GameMove(int SquareId);
 
-        public GameMove(int squareId)
-        {
-            SquareId = squareId;
-        }
-    }
-
-    public class PlayerStats
-    {
-        public int CorrectSequenceCount { get; set; }
-
-        public PlayerStats(int correctSequenceCount)
-        {
-            CorrectSequenceCount = correctSequenceCount;
-        }
-    }
+    public record PlayerStats(int CorrectSequenceCount);
 
     public partial class BlockGame : ComponentBase
     {
         // Static Random instance for randomness
         private static readonly Random random = new Random();
 
-        // Current sequence of squares for this round
         private List<GameMove> Sequence { get; set; } = new List<GameMove>();
 
         private PlayerStats playerStats = new PlayerStats(0);
@@ -44,19 +27,18 @@ namespace se_24.Components.Pages
 
         private GameState CurrentGameState { get; set; } = GameState.Waiting;
 
-        private int roundNumber = 1; // Track the current round number
+        private int roundNumber = 1;
 
         [Inject] private NavigationManager Navigation { get; set; }
 
         public void StartGame()
         {
             CurrentGameState = GameState.Started;
-            playerStats = new PlayerStats(0);  // Reset the player's correct sequence count
-            roundNumber = 1; // Start with the first round
+            playerStats = new PlayerStats(0);  // Reset the player's correct sequence count using 'record'
+            roundNumber = 1;
             StartNewRound();
         }
 
-        // When player clicks a square
         private async void OnPlayerClick(int squareId)
         {
             if (isAnimatingSequence || CurrentGameState != GameState.Started) return;
@@ -74,8 +56,8 @@ namespace se_24.Components.Pages
                 if (IsRoundComplete())
                 {
                     statusMessage = "Correct! Starting next round...";
-                    playerStats.CorrectSequenceCount++;  // Increment correct sequence count
-                    roundNumber++;  // Increment round number for the next round
+                    playerStats = playerStats with { CorrectSequenceCount = playerStats.CorrectSequenceCount + 1 };
+                    roundNumber++;
                     CurrentGameState = GameState.Finished;
                     await Task.Delay(1000);
                     StartNewRound();
@@ -89,16 +71,14 @@ namespace se_24.Components.Pages
             }
         }
 
-        // Generate a new random sequence for this round and animate it
         private async void StartNewRound()
         {
             isAnimatingSequence = true;
             statusMessage = "Watch the sequence...";
             CurrentGameState = GameState.Started;
 
-            GenerateNewRandomSequence(roundNumber); // Generate a new sequence based on the round number
+            GenerateNewRandomSequence(roundNumber);
 
-            // Play the sequence by lighting up the squares one by one
             foreach (var move in Sequence)
             {
                 activeSquare = move.SquareId;
@@ -117,11 +97,11 @@ namespace se_24.Components.Pages
         // Generate a new random sequence of squares for this round
         private void GenerateNewRandomSequence(int sequenceLength)
         {
-            Sequence.Clear(); // Clear the previous sequence
+            Sequence.Clear();
             for (int i = 0; i < sequenceLength; i++)
             {
-                int newSquareId = random.Next(1, 5); // Random number between 1 and 4
-                Sequence.Add(new GameMove(newSquareId)); // Add the new square to the sequence
+                int newSquareId = random.Next(1, 5);
+                Sequence.Add(new GameMove(newSquareId)); // Add the new square to the sequence using 'record'
             }
         }
 
