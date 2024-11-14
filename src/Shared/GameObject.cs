7b321a31e2a51cbc3c768db1f88b2548a5fecc72
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-11-14 20:10:51 +0200

    Added Id and reference to level to add relation between level and gameobject in ORM

--- src/Shared/GameObject.cs
+++ src/Shared/GameObject.cs
@@ -1,10 +1,14 @@
-﻿namespace src.Shared
+﻿using src.Games.FinderGame;
+
+namespace src.Shared
 {
     public class GameObject
     {
+        public int Id { get; set; }
         public string Name { get; set; } = "";
         public required string Image { get; set; }
         public required Position Position { get; set; } // GameObject position on screen (percentile)
         public bool IsFound { get; set; } = false;
+        public Level? Level { get; set; }
     }
 }
