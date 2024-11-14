d5c49c6403e33e6a11d1aee46dcc0611fa68b11c
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-11-14 20:12:22 +0200

    Changed array to list for entity framework to able to map entity

--- src/Games/ReadingGame/ReadingLevel.cs
+++ src/Games/ReadingGame/ReadingLevel.cs
@@ -2,9 +2,10 @@
 {
     public class ReadingLevel
     {
+        public int Id { get; set; }
         public int Level { get; set; }
         public string Text { get; set; } = "";
         public int ReadingTime { get; set; } = 60;
-        public ReadingQuestion[] Questions { get; set; } = new ReadingQuestion[0];
+        public List<ReadingQuestion> Questions { get; set; } = [];
     }
 }
