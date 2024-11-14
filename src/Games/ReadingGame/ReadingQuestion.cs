e858b660c177b19d5f9ae038966c7a7b624b53fa
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-11-14 20:11:38 +0200

    Added Id and reference to question to add relation between them

--- src/Games/ReadingGame/ReadingQuestion.cs
+++ src/Games/ReadingGame/ReadingQuestion.cs
@@ -2,9 +2,11 @@
 {
     public class ReadingQuestion
     {
+        public int Id { get; set; }
         public string Question { get; set; } = "";
         public string[] Answers { get; set; } = new string[4];
         public int CorrectAnswer { get; set; } = 0;
         public int Level { get; set; } = 1;
+        public ReadingLevel? ReadingLevel { get; set; }
     }
 }
