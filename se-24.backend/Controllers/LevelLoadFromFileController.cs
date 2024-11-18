d55611ca43cd768ead2784ce7f8cc5520113cbbf
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-11-18 18:24:56 +0200

    Added controllers to get levels for games, added DB context to allow interaction between app and DB, moved level files to backend, created initial migration

---
+++ se-24.backend/Controllers/LevelLoadFromFileController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using se_24.backend.src.Data;
+using se_24.backend.src.FileManipulation;
+using se_24.shared.src.Games.FinderGame;
+using se_24.shared.src.Games.ReadingGame;
+
+namespace se_24.backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LevelLoadFromFileController : ControllerBase
+    {
+        private readonly IDbContextFactory<AppDbContext> _dbFactory;
+        private readonly LevelLoader _levelLoader = new LevelLoader();
+        public LevelLoadFromFileController(IDbContextFactory<AppDbContext> DbFactory)
+        {
+            _dbFactory = DbFactory;
+        }
+
+        [HttpGet("FinderGameLevels")]
+        public ActionResult LoadFinderGameLevels()
+        {
+            using var dbContext = _dbFactory.CreateDbContext();
+            string path = "Files/Levels/FinderGame/";
+            List<Level> levels = _levelLoader.LoadAllLevels<Level>(path);
+            try
+            {
+                dbContext.FinderLevels.AddRange(levels);
+                dbContext.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Failed to save levels: " + ex.Message + '\n' + ex.InnerException);
+                return BadRequest("Failed to save levels: " + ex.Message + '\n' + ex.InnerException);
+            }
+            
+            return Ok("Added " + levels.Count + " levels");
+        }
+
+        [HttpGet("ReadingGameLevels")]
+        public ActionResult LoadReadingGameLevels()
+        {
+            using var dbContext = _dbFactory.CreateDbContext();
+            string path = "Files/Levels/ReadingGame/";
+            List<ReadingLevel> levels = _levelLoader.LoadAllLevels<ReadingLevel>(path);
+            try
+            {
+                dbContext.ReadingLevels.AddRange(levels);
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to save levels: " + ex.Message + '\n' + ex.InnerException);
+                return BadRequest("Failed to save levels: " + ex.Message + '\n' + ex.InnerException);
+            }
+            return Ok("Added " + levels.Count + " levels");
+        }
+    }
+}
---
+++ se-24.backend/Migrations/20241118161243_InitialMigration.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace se_24.backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FinderLevels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Difficulty = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: false),
+                    GivenTime = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinderLevels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReadingLevels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Level = table.Column<int>(type: "integer", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    ReadingTime = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReadingLevels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FinderLevelGameObjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: false),
+                    Position_X = table.Column<int>(type: "integer", nullable: false),
+                    Position_Y = table.Column<int>(type: "integer", nullable: false),
+                    IsFound = table.Column<bool>(type: "boolean", nullable: false),
+                    LevelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinderLevelGameObjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FinderLevelGameObjects_FinderLevels_LevelId",
+                        column: x => x.LevelId,
+                        principalTable: "FinderLevels",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReadingQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Question = table.Column<string>(type: "text", nullable: false),
+                    Answers = table.Column<string[]>(type: "text[]", nullable: false),
+                    CorrectAnswer = table.Column<int>(type: "integer", nullable: false),
+                    Level = table.Column<int>(type: "integer", nullable: false),
+                    ReadingLevelId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReadingQuestions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReadingQuestions_ReadingLevels_ReadingLevelId",
+                        column: x => x.ReadingLevelId,
+                        principalTable: "ReadingLevels",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinderLevelGameObjects_LevelId",
+                table: "FinderLevelGameObjects",
+                column: "LevelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReadingQuestions_ReadingLevelId",
+                table: "ReadingQuestions",
+                column: "ReadingLevelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinderLevelGameObjects");
+
+            migrationBuilder.DropTable(
+                name: "ReadingQuestions");
+
+            migrationBuilder.DropTable(
+                name: "FinderLevels");
+
+            migrationBuilder.DropTable(
+                name: "ReadingLevels");
+        }
+    }
+}
