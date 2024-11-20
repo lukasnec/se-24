3eb4913d9144507430d55d3e566a0d0c8109a1f3
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-11-20 03:12:38 +0200

    Created username generator to generate names for guests

---
+++ se-24.shared/src/Shared/UsernameGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace se_24.shared.src.Shared
+{
+    public class UsernameGenerator
+    {
+        public string GenerateGuestName()
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(100000, 1000000); // Generates a random 6 digit number
+            return "guest-" + number;
+        }
+    }
+}
