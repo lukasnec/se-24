3d6c7d106eafa563f26a62e6109533c34c45f441
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-11-20 15:04:19 +0200

    Created se-24.tests project, created some tests, generated coverage-report

---
+++ se-24.tests/Tests/FinderGameTests.cs
@@ -0,0 +1,74 @@
+﻿using Bunit;
+using Moq;
+using se_24.frontend.Components.Pages;
+using se_24.shared.src.Shared;
+using se_24.shared.src.Games.FinderGame;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using src.Enums;
+using System.Net.Http.Json;
+
+public class FinderGameTests : TestContext
+{
+    [Fact]
+    public void GameState_ShouldBeWaiting_WhenComponentIsInitialized()
+    {
+        var component = RenderComponent<FinderGame>();
+        Assert.Equal(GameState.Waiting, component.Instance.GetCurrentGameState());
+    }
+
+    [Fact]
+    public void StartGame_ShouldChangeGameStateToStarted()
+    {
+        var component = RenderComponent<FinderGame>();
+
+        component.Instance.StartGame();
+
+        Assert.Equal(GameState.Started, component.Instance.GetCurrentGameState());
+    }
+
+    [Fact]
+    public void ObjectClicked_ShouldIncreaseObjectsFound()
+    {
+        var gameObject = new GameObject {
+            Image = "images/findergame/pixelated-santa-large.png",
+            IsFound = false,
+            Position = new Position { X = 50, Y = 50 } };
+        var component = RenderComponent<FinderGame>();
+
+        component.Instance.ObjectClicked(gameObject);
+
+        Assert.True(gameObject.IsFound); // Object should be marked as found
+        Assert.Equal(1, component.Instance.objectsFound); // One object should be found
+    }
+
+    [Fact]
+    public void CalculateScore_ShouldCalculateCorrectly()
+    {
+        var component = RenderComponent<FinderGame>();
+
+        // Test for "easy"
+        component.Instance.selectedDifficulty = "easy";
+        component.Instance.totalGivenTime = 100;
+        component.Instance.totalElapsedTime = 80;
+        component.Instance.CalculateScore();
+
+        Assert.Equal(20, component.Instance.score); // Easy should multiply by 1
+
+        // Test for "medium"
+        component.Instance.selectedDifficulty = "medium";
+        component.Instance.CalculateScore();
+        Assert.Equal(40, component.Instance.score); // Medium should multiply by 2
+
+        // Test for "hard"
+        component.Instance.selectedDifficulty = "hard";
+        component.Instance.CalculateScore();
+        Assert.Equal(60, component.Instance.score); // Hard should multiply by 3
+    }
+}
