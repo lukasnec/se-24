3fe873448208d7fd0bc22f369b876bbaebaf9858
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-11-20 12:33:32 +0200

    Created Leaderboard component

---
+++ se-24.frontend/Components/Pages/Leaderboard.razor.cs
@@ -0,0 +1,53 @@
+﻿using se_24.shared.src.Games.FinderGame;
+using se_24.shared.src.Games.ReadingGame;
+using se_24.shared.src.Shared;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace se_24.frontend.Components.Pages
+{
+    public partial class Leaderboard
+    {
+        public List<Score> Scores { get; set; } = new();
+        public string selectedGameName = "All";
+        public int topRanks = 0;
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            Scores = await GetScoresAsync();
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public async Task<List<Score>> GetScoresAsync()
+        {
+            string url = $"https://localhost:7077/api/Score";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                    return JsonSerializer.Deserialize<List<Score>>(jsonResponse, options);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching scores: {ex.Message}");
+                throw;
+            }
+        }
+
+        private IEnumerable<Score> FilteredScores => Scores
+        .Where(s => selectedGameName == "All" || s.GameName == selectedGameName)
+        .OrderByDescending(s => s.value)
+        .Take(topRanks > 0 ? topRanks : Scores.Count);
+
+    }
+}
