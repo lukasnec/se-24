d52553f2da9ef9fc95ebba864eb18720d33149e7
Matas Sipavičius <matas.sipavicius@mif.stud.vu.lt>
2024-11-27 13:56:03 +0200

    Fixed ScoreAddition Migration

---
+++ se-24.backend/Migrations/20241127115447_ScoreAddition.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace se_24.backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScoreAddition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Scores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PlayerName = table.Column<string>(type: "text", nullable: false),
+                    GameName = table.Column<string>(type: "text", nullable: false),
+                    value = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scores", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scores");
+        }
+    }
+}
